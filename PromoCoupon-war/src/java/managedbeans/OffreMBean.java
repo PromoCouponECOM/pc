c5b6f2e52b828e35e6cfa9cafa53700c88b73f59
Zalgo <Zalgo@130.190.29.153>
2013-12-18 11:02:13 +0100

    integration antho

--- PromoCoupon-war/src/java/managedbeans/OffreMBean.java
+++ PromoCoupon-war/src/java/managedbeans/OffreMBean.java
@@ -6,36 +6,38 @@ package managedbeans;
 
 import entities.Offre;
 import java.io.Serializable;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 import session.OffreManager;
-import javax.enterprise.context.RequestScoped;
 
 /**
  *
  * @author John624
  */
 @Named(value="offreMBean")
-@RequestScoped
+@SessionScoped
 public class OffreMBean implements Serializable{
     private List<Offre> offres;
     @EJB
     private OffreManager offreManager;
     private Offre offre;
-    
-    private String type;
+    private Offre selectedOffre;  
     /**
      * Creates a new instance of OffreMBean
      */
     
+   
     public OffreMBean() {
         Offre offre = new Offre();
     }
     
     public List<Offre> getOffres(){
-        if((offres == null) || (offres.isEmpty()))
+    if((offres == null) || (offres.isEmpty()))
             offres = offreManager.getAllOffre();  
         return offreManager.getAllOffre(); 
     }
@@ -65,15 +67,62 @@ public class OffreMBean implements Serializable{
         return "OffreList";  
     }
     
-    public List<Offre> showTypeOffre(String type){
-          return offreManager.getSpecialOffres(type);
-    }
+    
+    public Offre getSelectedOffre() {  
+        return selectedOffre;  
+    }  
+  
+    public void setSelectedOffre(Offre selectedOffre) {  
+        this.selectedOffre = selectedOffre;  
+    } 
+    
+    public String calculerPourcentage(double prixActuel, double prixOrigin) {  
+        double res = Math.round(100 - (prixActuel*100/prixOrigin));
+        return "-"+res+"%";
+    } 
+    
+     public String dateValidite(Date dateDebut, Date dateFin) { 
+        GregorianCalendar cal = new GregorianCalendar();
+        cal.setTime(dateDebut);
 
-    public String getType() {
-        return type;
-    }
+        int jourDeb = cal.get(Calendar.DATE);
+        String jourDebut;
+        if (jourDeb<10) {
+            jourDebut = "0"+jourDeb;
+        }
+        else { 
+            jourDebut = ""+jourDeb;
+        }
+        int moisDeb = cal.get(Calendar.MONTH);
+        String moisDebut;
+        if (moisDeb<10) {
+            moisDebut = "0"+jourDeb;
+        }
+        else { 
+            moisDebut = ""+jourDeb;
+        }
+        int anneeDebut = cal.get(Calendar.YEAR);
+        
+        cal.setTime(dateFin);
+        int jourF = cal.get(Calendar.DATE);
+        String jourFin;
+        if (jourF<10) {
+            jourFin = "0"+jourDeb;
+        }
+        else {
+            jourFin = ""+jourDeb;
+        }
+        int moisF = cal.get(Calendar.MONTH);
+        String moisFin;
+        if (moisF<10) {
+            moisFin = "0"+jourDeb;
+        }
+        else { 
+            moisFin = ""+jourDeb;
+        }
+        int anneeFin = cal.get(Calendar.YEAR);
 
-    public void setType(String type) {
-        this.type = type;
-    }
+        
+        return "3";
+    } 
 }
